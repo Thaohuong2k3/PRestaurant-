8162e313509bbf5e09c8c44bed3dc16d686e8219
Thaohuong2k3 <thaohuongcute1412@gmail.com>
2025-07-13 20:57:53 +0700

    Mẫu Login sang https

--- app/src/main/java/com/example/porestaurant/network/ApiClient.java
+++ app/src/main/java/com/example/porestaurant/network/ApiClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "http://restaurantbe.runasp.net/api/";
+    private static final String BASE_URL = "https://restaurantbe.runasp.net/api/";
     private static Retrofit retrofit;
 
     public static Retrofit getClient() {
