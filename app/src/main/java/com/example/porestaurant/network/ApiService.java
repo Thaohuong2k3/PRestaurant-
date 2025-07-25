2078ba22f9cd87d698fe48229a4031c731e39919
Long Nguyễn <huylong.tk123@gmail.com>
2025-07-25 12:37:51 +0700

    Update ApiService.java

--- app/src/main/java/com/example/porestaurant/network/ApiService.java
+++ app/src/main/java/com/example/porestaurant/network/ApiService.java
@@ -112,7 +112,7 @@ public interface ApiService {
     @PUT("tables/{id}/cancel")
     Call<TableDTO> cancelTable(@Path("id") int id);
 
-    @GET("tables") 
+    @GET("tables")
     Call<List<TableDTO>> getAllTables();
 
     // ====== MAP ENDPOINTS ======
